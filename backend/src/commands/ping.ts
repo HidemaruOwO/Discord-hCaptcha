092d4a3b2baf7b610d755b720a3d027d195d279f
HidemaruOwO <hidemal@outlook.jp>
2022-08-28 22:57:24 +0900

    :sparkles: (backend/src/commands/ping.ts) Pingコマンドの追加

---
+++ backend/src/commands/ping.ts
@@ -0,0 +1,49 @@
+import { Client, BaseCommandInteraction } from "discord.js";
+import { Command } from "../interface";
+
+export const Ping: Command = {
+  data: {
+    name: "ping",
+    description: "Botのレイテンシを表示します",
+  },
+  async execute(client: Client, interaction: BaseCommandInteraction) {
+    const emoji = ["🟢", "🟡", "🔴"];
+
+    let websocketPing: number = client.ws.ping;
+    let websocketPingMsg: string = "";
+
+    if (websocketPing < 140) {
+      websocketPingMsg = `${emoji[0]} ${websocketPing}ミリ秒`;
+    } else if (websocketPing > 140 && websocketPing < 200) {
+      websocketPingMsg = `${emoji[1]} ${websocketPing}ミリ秒`;
+    } else if (websocketPing > 200) {
+      websocketPingMsg = `${emoji[2]} ${websocketPing}ミリ秒`;
+    }
+
+    let apiLatency: number = Date.now() - interaction.createdTimestamp;
+    let apiLatencyMsg: string = "";
+
+    if (apiLatency < 140) {
+      apiLatencyMsg = `${emoji[0]} ${apiLatency}ミリ秒`;
+    } else if (apiLatency > 140 && apiLatency < 200) {
+      apiLatencyMsg = `${emoji[1]} ${apiLatency}ミリ秒`;
+    } else if (apiLatency > 200) {
+      apiLatencyMsg = `${emoji[2]} ${apiLatency}ミリ秒`;
+    }
+
+    await interaction.reply({
+      embeds: [
+        {
+          title: ":ping_pong: Ping!!Pong!!",
+          description: "Ping値を計測してきました!!",
+          fields: [
+            { name: "WebSocket Speed", value: websocketPingMsg },
+            { name: "APIレイテンシ", value: apiLatencyMsg },
+          ],
+          color: "RANDOM",
+          timestamp: new Date(),
+        },
+      ],
+    });
+  },
+};
