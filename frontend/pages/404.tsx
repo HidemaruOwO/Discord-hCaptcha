4f7a3b19740049c379ae47795d22c786896f3495
HidemaruOwO <hidemal@outlook.jp>
2022-09-06 04:50:19 +0900

    :snowman: /authの仕様変更 (frontend/components/Auth.tsx, frontend/components/Title.tsx, frontend/components/subtitle.tsx, frontend/pages/404.tsx, frontend/pages/auth/index.tsx, frontend/styles/404.module.css, frontend/styles/Auth.module.css)

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,34 +1,16 @@
-import Auth from "../components/Auth";
-import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-import styles from "../styles/404.module.css";
-
 export default function NotFound() {
-  const [serverId, setServerId] = React.useState<string>("");
-  useEffect(() => {
-    setServerId(window.location.pathname.split("/")[1]);
-    console.log("serveId: " + serverId);
-  }, []);
-
-  if (serverId.match(/^[0-9]+$/)) {
-    return (
-      <div className={styles.background}>
-        <Auth serverId={serverId} />
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <Container>
-          <Row>
-            <Col className="text-center">
-              <h1>404</h1>
-              <p>{serverId} is not the discord server id.</p>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="text-center">
+            <h1>404</h1>
+            <p>not found.</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
