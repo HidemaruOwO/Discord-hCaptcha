66e34bf11e76b53b1a8f6616d3bf374334e3aeb2
HidemaruOwO <hidemal@outlook.jp>
2022-09-14 14:29:41 +0900

    :sparkles: 認証ボタンを作成するコマンドの追加 (backend/src/commands/genbutton.ts)

---
+++ backend/src/commands/genbutton.ts
@@ -0,0 +1,50 @@
+import {
+  Client,
+  BaseCommandInteraction,
+  MessageButton,
+  MessageActionRow,
+} from "discord.js";
+import { Command } from "../interface";
+
+const cmd: Command = {
+  data: {
+    name: "genbutton",
+    description: "認証ボタンを作成します",
+    defaultMemberPermissions: ["ADMINISTRATOR"],
+  },
+  async execute(client: Client, interaction: BaseCommandInteraction) {
+    const guild = interaction.guild;
+    await interaction.reply({
+      content: "ボタンを作成します",
+      ephemeral: true,
+    });
+    const role = guild?.roles.cache.find((role) => role.name === "verified");
+    if (role === undefined) {
+      await interaction.editReply({
+        content: "Verifiedロールが存在しません",
+      });
+    }
+    const button = new MessageButton()
+      .setCustomId("verify")
+      .setStyle("PRIMARY")
+      .setLabel("認証")
+      .setEmoji("✅");
+    await interaction.channel?.send({
+      embeds: [
+        {
+          title: "認証方法",
+          description: "認証を行うには、以下のボタンを押してください",
+          color: "RANDOM",
+          timestamp: new Date(),
+          footer: {
+            text: "©️ HidemaruOwO | Discord hCaptcha",
+          },
+        },
+      ],
+      components: [new MessageActionRow().addComponents(button)],
+    });
+    await interaction.editReply({ content: "ボタンを作成しました" });
+  },
+};
+
+export { cmd };
