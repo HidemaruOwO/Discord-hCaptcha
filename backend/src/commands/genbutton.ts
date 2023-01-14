c23cdcb0b5e8bb51bbd59f1c4e2a0d3b8fe2e8c0
HidemaruOwO <hidemal@outlook.jp>
2023-01-14 18:46:43 +0900

    :bug: コマンドの条件分岐をしっかり動作するように修正 (backend/src/commands/genbutton.ts backend/src/commands/genrole.ts backend/src/discord.ts)

--- backend/src/commands/genbutton.ts
+++ backend/src/commands/genbutton.ts
@@ -1,51 +1,52 @@
 import {
-  Client,
-  BaseCommandInteraction,
-  MessageButton,
-  MessageActionRow,
+   Client,
+   BaseCommandInteraction,
+   MessageButton,
+   MessageActionRow,
 } from "discord.js";
 import { Command } from "../interface";
 
 const cmd: Command = {
-  data: {
-    name: "genbutton",
-    description: "認証ボタンを作成します",
-    defaultMemberPermissions: ["ADMINISTRATOR"],
-  },
-  async execute(client: Client, interaction: BaseCommandInteraction) {
-    const guild = interaction.guild;
-    await interaction.reply({
-      content: "ボタンを作成します",
-      ephemeral: true,
-    });
-    const role = guild?.roles.cache.find((role) => role.name === "verified");
-    if (role === undefined) {
-      await interaction.editReply({
-        content: "Verifiedロールが存在しません",
+   data: {
+      name: "genbutton",
+      description: "認証ボタンを作成します",
+      defaultMemberPermissions: ["ADMINISTRATOR"],
+   },
+   async execute(client: Client, interaction: BaseCommandInteraction) {
+      const guild = interaction.guild;
+      await interaction.reply({
+         content: "ボタンを作成します",
+         ephemeral: true,
       });
-      return;
-    }
-    const button = new MessageButton()
-      .setCustomId("verify")
-      .setStyle("PRIMARY")
-      .setLabel("認証")
-      .setEmoji("✅");
-    await interaction.channel?.send({
-      embeds: [
-        {
-          title: "認証方法",
-          description: "認証を行うには、以下のボタンを押してください",
-          color: "RANDOM",
-          timestamp: new Date(),
-          footer: {
-            text: "©️ HidemaruOwO | Discord hCaptcha",
-          },
-        },
-      ],
-      components: [new MessageActionRow().addComponents(button)],
-    });
-    await interaction.editReply({ content: "ボタンを作成しました" });
-  },
+      const role = guild?.roles.cache.find((role) => role.name === "verified");
+      if (role === void 0) {
+         await interaction.editReply({
+            content:
+               "Verifiedロールが存在しません\n`/genrole`コマンドを実行して再度お試しください",
+         });
+         return;
+      }
+      const button = new MessageButton()
+         .setCustomId("verify")
+         .setStyle("PRIMARY")
+         .setLabel("認証")
+         .setEmoji("✅");
+      await interaction.channel?.send({
+         embeds: [
+            {
+               title: "認証方法",
+               description: "認証を行うには、以下のボタンを押してください",
+               color: "RANDOM",
+               timestamp: new Date(),
+               footer: {
+                  text: "©️ HidemaruOwO | Discord hCaptcha",
+               },
+            },
+         ],
+         components: [new MessageActionRow().addComponents(button)],
+      });
+      await interaction.editReply({ content: "ボタンを作成しました" });
+   },
 };
 
 export { cmd };
--- backend/src/commands/genrole.ts
+++ backend/src/commands/genrole.ts
@@ -2,30 +2,40 @@ import { Client, BaseCommandInteraction } from "discord.js";
 import { Command } from "../interface";
 
 const cmd: Command = {
-  data: {
-    name: "genrole",
-    description: "Verifiedロールを作成します",
-    defaultMemberPermissions: ["ADMINISTRATOR"],
-  },
-  async execute(client: Client, interaction: BaseCommandInteraction) {
-    const guild = interaction.guild;
-    await interaction.reply({
-      content: "ロールを作成します",
-      ephemeral: true,
-    });
-    const role = guild?.roles.cache.find((role) => role.name === "verified");
-    console.log(role);
-    if (!role === undefined) {
-      await interaction.editReply({
-        content: "Verifiedロールが存在します",
+   data: {
+      name: "genrole",
+      description: "Verifiedロールを作成します",
+      defaultMemberPermissions: ["ADMINISTRATOR"],
+   },
+   async execute(client: Client, interaction: BaseCommandInteraction) {
+      const guild = interaction.guild;
+      await interaction.reply({
+         content: "ロールを作成します",
+         ephemeral: true,
       });
-      return;
-    }
-    guild?.roles.create({ name: "verified" });
-    await interaction.editReply({
-      content: "ロールを作成しました",
-    });
-  },
+
+      const role = guild?.roles.cache.find((role) => role.name === "verified");
+      if (role === void 0) {
+         try {
+            guild?.roles.create({ name: "verified" });
+         } catch (err) {
+            await interaction.editReply({
+               content:
+                  "ロールの作成に失敗しました\n権限が不足している可能性があります",
+            });
+            return;
+         }
+
+         await interaction.editReply({
+            content: "ロールを作成しました",
+         });
+      } else {
+         await interaction.editReply({
+            content: "Verifiedロールが存在します",
+         });
+         return;
+      }
+   },
 };
 
 export { cmd };
