e41e5986b2a4c27d40e979d1fab82e02357ca77b
HidemaruOwO <hidemal@outlook.jp>
2023-01-22 19:02:44 +0900

    :bug: /setupコマンドでverifiedロールが存在する場合にsetupコマンドを中止するように修正 (backend/src/commands/setup.ts)

--- backend/src/commands/setup.ts
+++ backend/src/commands/setup.ts
@@ -1,56 +1,56 @@
 import {
-  Client,
-  BaseCommandInteraction,
-  MessageButton,
-  MessageActionRow,
+   Client,
+   BaseCommandInteraction,
+   MessageButton,
+   MessageActionRow,
 } from "discord.js";
 import { Command } from "../interface";
 
 const cmd: Command = {
-  data: {
-    name: "setup",
-    description: "Discord hCaptch認証のセットアップをします",
-    defaultMemberPermissions: ["ADMINISTRATOR"],
-  },
-  async execute(client: Client, interaction: BaseCommandInteraction) {
-    const guild = interaction.guild;
-    await interaction.reply({
-      content: "セットアップを開始します",
-      ephemeral: true,
-    });
-    const role = guild?.roles.cache.find((role) => role.name === "verified");
-    if (!role === undefined) {
-      await interaction.editReply({
-        content:
-          "Verifiedロールが存在します\n認証ボタンを作成する場合は`/genbutton`コマンドを実行してください",
+   data: {
+      name: "setup",
+      description: "Discord hCaptch認証のセットアップをします",
+      defaultMemberPermissions: ["ADMINISTRATOR"],
+   },
+   async execute(client: Client, interaction: BaseCommandInteraction) {
+      const guild = interaction.guild;
+      await interaction.reply({
+         content: "セットアップを開始します",
+         ephemeral: true,
       });
-      return;
-    }
-    guild?.roles.create({ name: "verified" });
-    await interaction.editReply({
-      content: "ロールを作成しました",
-    });
-    const button = new MessageButton()
-      .setCustomId("verify")
-      .setStyle("PRIMARY")
-      .setLabel("認証")
-      .setEmoji("✅");
-    await interaction.channel?.send({
-      embeds: [
-        {
-          title: "認証方法",
-          description: "認証を行うには、以下のボタンを押してください",
-          color: "RANDOM",
-          timestamp: new Date(),
-          footer: {
-            text: "©️ HidemaruOwO | Discord hCaptcha",
-          },
-        },
-      ],
-      components: [new MessageActionRow().addComponents(button)],
-    });
-    await interaction.editReply({ content: "セットアップを完了しました" });
-  },
+      const role = guild?.roles.cache.find((role) => role.name === "verified");
+      if (!role === void 0) {
+         await interaction.editReply({
+            content:
+               "Verifiedロールが存在します\n認証ボタンを作成する場合は`/genbutton`コマンドを実行してください",
+         });
+         return;
+      }
+      guild?.roles.create({ name: "verified" });
+      await interaction.editReply({
+         content: "ロールを作成しました",
+      });
+      const button = new MessageButton()
+         .setCustomId("verify")
+         .setStyle("PRIMARY")
+         .setLabel("認証")
+         .setEmoji("✅");
+      await interaction.channel?.send({
+         embeds: [
+            {
+               title: "認証方法",
+               description: "認証を行うには、以下のボタンを押してください",
+               color: "RANDOM",
+               timestamp: new Date(),
+               footer: {
+                  text: "©️ HidemaruOwO | Discord hCaptcha",
+               },
+            },
+         ],
+         components: [new MessageActionRow().addComponents(button)],
+      });
+      await interaction.editReply({ content: "セットアップを完了しました" });
+   },
 };
 
 export { cmd };
